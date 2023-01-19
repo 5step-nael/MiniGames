5c78cce1d86481a2d5ee09642f31a6eb6e99ebf7
coder-lloyd <86070086+coder-lloyd@users.noreply.github.com>
2023-01-20 00:59:04 +0900

    [RPS] proj-cocos
    ## assets
    + nodes: Card

---
+++ RPS/proj-cocos/RPS/assets/nodes/Card.ts
@@ -0,0 +1,56 @@
+import { _decorator, Component, Node, Sprite, Color, SpriteFrame, color } from 'cc';
+import { Commons } from '../scripts/Defines';
+const { ccclass, property } = _decorator;
+
+@ccclass('Card')
+export class Card extends Component {
+    @property(Sprite)
+    spr_Outline: Sprite = null!;
+
+    @property(Sprite)
+    spr_RPSIcon: Sprite = null!;
+
+    @property([SpriteFrame])
+    Sprites_Icon: SpriteFrame[] = []!;
+
+    _kind: Commons.Kind = Commons.Kind.unknown;
+
+    start() {
+        this.Setup(Commons.Kind.scissors, false);
+    }
+
+    public Setup = (__kind: Commons.Kind, __isUnknown: boolean = false) => {
+        this._kind = __kind;
+
+        let colorKind = __kind;
+        {
+            if (__isUnknown) {
+                colorKind = Commons.Kind.unknown;
+            }
+            this.spr_Outline.color = this.Get_Color(colorKind);
+        }
+
+        this.Setup_RPSIcon(__kind);
+    }
+
+    Get_Color = (__kind: Commons.Kind): Color => {
+        let ret: Color = new Color(64, 64, 64);//Commons.Kind.unknown
+        if(Commons.Kind.rock == __kind) {
+            ret = new Color(104, 150, 255);
+        }
+        else if(Commons.Kind.paper == __kind) {
+            ret = new Color(255, 212, 114);
+        }
+        else if(Commons.Kind.scissors == __kind) {
+            ret = new Color(255, 107, 88);
+        }
+        return ret;
+    }
+
+    Setup_RPSIcon = (__kind: Commons.Kind) => {
+        this.spr_RPSIcon.spriteFrame = this.Sprites_Icon[__kind];
+    }
+
+    // update(deltaTime: number) {}
+}
+

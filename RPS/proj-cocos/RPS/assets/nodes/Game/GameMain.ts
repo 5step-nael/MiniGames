1549d7ae96e06cf574d5139d1198fec39cf74c97
coder-lloyd <86070086+coder-lloyd@users.noreply.github.com>
2023-01-20 03:09:11 +0900

    [RPS] proj-cocos
    ## assets
    + scripts/Defines: Result
     - 게임결과 enum 추가

--- RPS/proj-cocos/RPS/assets/nodes/Game/GameMain.ts
+++ RPS/proj-cocos/RPS/assets/nodes/Game/GameMain.ts
@@ -27,16 +27,67 @@ export class GameMain extends Component {
         this.cards[__index].Setup(__index, info);
     }
 
-    private Check(__kind: Commons.Kind) {        
-        let isCollect = (this.cards[0].Get_Info().kind == __kind);
-        // console.log(`Check(${__kind}) => isCollect= ${isCollect}`);
+    private Check(__kind: Commons.Kind) {
+        // let isCollect = (this.cards[0].Get_Info().kind == __kind);
+        // // console.log(`Check(${__kind}) => isCollect= ${isCollect}`);
 
-        if(!isCollect) {
-            console.log("GameOver!!");
-            return;
+        // if(!isCollect) {
+        //     console.log("GameOver!!");
+        //     return;
+        // }
+
+        let cardKind = this.cards[0].Get_Info().kind;
+        let userKind = __kind;
+        let result: Commons.Result = Commons.Result.tie;
+        // if(Commons.Kind.rock == userKind) {
+        //     if(Commons.Kind.scissors == cardKind) {
+        //         result = Commons.Result.win;
+        //     }
+        //     else if(Commons.Kind.paper == cardKind) {
+        //         result = Commons.Result.lose;
+        //     }
+        // }
+        // else if(Commons.Kind.paper == userKind) {
+        //     if(Commons.Kind.rock == cardKind) {
+        //         result = Commons.Result.win;
+        //     }
+        //     else if(Commons.Kind.scissors == cardKind) {
+        //         result = Commons.Result.lose;
+        //     }
+        // }
+        // else if(Commons.Kind.scissors == userKind) {
+        //     if(Commons.Kind.paper == cardKind) {
+        //         result = Commons.Result.win;
+        //     }
+        //     else if(Commons.Kind.rock == cardKind) {
+        //         result = Commons.Result.lose;
+        //     }
+        // }
+        if(userKind != cardKind) {
+            let loseCardKind = this.Get_WinKind(userKind);
+            result = (loseCardKind == cardKind
+                ? Commons.Result.win
+                : Commons.Result.lose
+                );
         }
 
-        console.log("collect~~");
+        {//DEV
+            let strResult = "";
+            if(Commons.Result.lose == result) {
+                strResult = "졌다";
+            }
+            // else if(Commons.Result.win == result) {
+            //     strResult = "이겼다";
+            // }
+            else if(Commons.Result.tie == result) {
+                strResult = "비겼다";
+            }
+
+            if (Commons.Result.win != result) {
+                console.log(`Check: ${strResult}`);
+                return;
+            }
+        }
 
         let infos: CardInfo[] = [];
         for(let n=0; n<this.cards.length; n++) {
@@ -49,6 +100,20 @@ export class GameMain extends Component {
             this.cards[n].Setup(n, infos[n + 1]);
         }
         this.RandomCard(lastIndex);
+    }
+
+    private Get_WinKind(__userKind: Commons.Kind): Commons.Kind {//인자로 받은 Kind가 이기는 상대방의 Kind
+        let ret = Commons.Kind.unknown;
+        if(Commons.Kind.rock == __userKind) {
+            ret = Commons.Kind.scissors;
+        }
+        else if(Commons.Kind.paper == __userKind) {
+            ret = Commons.Kind.rock;
+        }
+        else if(Commons.Kind.scissors == __userKind) {
+            ret = Commons.Kind.paper;
+        }
+        return ret;
     }
 
     OnClick_Action(event: Event, customEventData: string) {
--- RPS/proj-cocos/RPS/assets/scripts/Defines.ts
+++ RPS/proj-cocos/RPS/assets/scripts/Defines.ts
@@ -1,5 +1,9 @@
 export namespace Commons {
     export enum Kind { unknown = -1,
-        rock = 0, paper, scissors
+        rock = 0, paper, scissors,
+    }
+
+    export enum Result { NONE = -1,
+        lose = 0, win, tie,
     }
 }

c87c9b975168275ea1b4cc4f809b21aaa02c895a
coder-lloyd <86070086+coder-lloyd@users.noreply.github.com>
2023-01-20 00:16:20 +0900

    [RPS] proj-cocos
    ## nodes
    + GameMain 추가

---
+++ RPS/proj-cocos/RPS/assets/nodes/Game/GameMain.ts
@@ -0,0 +1,12 @@
+import { _decorator, Component, Node } from 'cc';
+import { Card } from '../Card';
+const { ccclass, property } = _decorator;
+
+@ccclass('GameMain')
+export class GameMain extends Component {
+    start() {
+    }
+
+    // update(deltaTime: number) {}
+}
+

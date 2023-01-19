0743270054407c0394d4d1dcf006bbd078b5b6d9
coder-lloyd <86070086+coder-lloyd@users.noreply.github.com>
2023-01-20 05:00:03 +0900

    [RPS] proj-cocos
    ## assets
    + nodes/Game/ActionButton: 전용 프리팹

---
+++ RPS/proj-cocos/RPS/assets/nodes/Game/ActionButton.ts
@@ -0,0 +1,12 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('ActionButton')
+export class ActionButton extends Component {
+    start() {
+
+    }
+
+    // update(deltaTime: number) {}
+}
+

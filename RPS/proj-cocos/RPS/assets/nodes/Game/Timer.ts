6299b655acbc00cbe626442581dedfd0f6d76126
coder-lloyd <86070086+coder-lloyd@users.noreply.github.com>
2023-01-24 16:30:29 +0900

    [RPS] proj-cocos ## assets/nodes/Game/Timer: 종료 콜백처리

--- RPS/proj-cocos/RPS/assets/nodes/Game/Timer.ts
+++ RPS/proj-cocos/RPS/assets/nodes/Game/Timer.ts
@@ -8,10 +8,34 @@ export class Timer extends Component {
 
     private _isRunning = false;
 
+    private _cb_EndTime: () => void;
+
     start() {
         input.on(Input.EventType.KEY_DOWN, this.onKeyDown, this);
 
+        this.Reset_Timer();
+
+        { this.Setup_Timer(this.Callback) }//DEV
+    }
+
+    private Reset_Timer = () => {
+        this._isRunning = false;
+
         this.spr_Inner.fillRange = 1.0;
+    }
+
+    Setup_Timer = (__callback_EndTime: () => void) => {
+        this._cb_EndTime = __callback_EndTime;
+        this.Reset_Timer();
+    }
+
+    Control_Timer = (__running: boolean) => {
+        this._isRunning = __running;
+    }
+
+    //begin of DEV DEV
+    private Callback = () => {
+        console.log("Callback");
     }
 
     onKeyDown (event: EventKeyboard) {
@@ -19,7 +43,8 @@ export class Timer extends Component {
             case KeyCode.ENTER:
             case KeyCode.NUM_ENTER:
                 // console.log('Press a key');
-                this._isRunning = !this._isRunning;
+                // this._isRunning = !this._isRunning;
+                this.Control_Timer(!this._isRunning);
                 break;
 
             case KeyCode.SPACE:
@@ -27,6 +52,7 @@ export class Timer extends Component {
                 break;
         }
     }
+    //end of DEV DEV
 
     update(deltaTime: number) {
         // console.log(deltaTime);
@@ -41,9 +67,18 @@ export class Timer extends Component {
 
         let timeRange: number = this.spr_Inner.fillRange;
         timeRange -= (__dt * 1.0);
+
+        let isEnded = false;
         if(0.0 >= timeRange) {
             timeRange = 0.0;
+            this._isRunning = false;
+            
+            isEnded = true;
         }
         this.spr_Inner.fillRange = timeRange;
+
+        if(isEnded && null !=  this._cb_EndTime) {
+            this._cb_EndTime();
+        }
     }
 }

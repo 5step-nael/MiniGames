0e00b55bf33c30dc580a068f020dc96c6e99dec9
coder-lloyd <86070086+coder-lloyd@users.noreply.github.com>
2023-01-24 18:04:51 +0900

    [RPS] proj-cocos ## assets/scripts/Defines: Option 추가

--- RPS/proj-cocos/RPS/assets/scripts/Defines.ts
+++ RPS/proj-cocos/RPS/assets/scripts/Defines.ts
@@ -3,6 +3,10 @@ export namespace Commons {
         rock = 0, paper, scissors,
     }
 
+    export enum Option { NONE = -1,
+        def = 0, unknown, lose,
+    }
+
     export enum Result { NONE = -1,
         lose = 0, win, tie,
     }

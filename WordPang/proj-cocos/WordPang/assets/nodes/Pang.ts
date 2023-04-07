e20ac201d5b850be7df822532a757f9a7089f9aa
coder-lloyd <86070086+coder-lloyd@users.noreply.github.com>
2023-04-07 19:57:47 +0900

    [WordPang - cocos] ## 게임구현 : 팡팡 + Scene - Main  - Pang 충돌 체크

--- WordPang/proj-cocos/WordPang/assets/nodes/Pang.ts
+++ WordPang/proj-cocos/WordPang/assets/nodes/Pang.ts
@@ -18,5 +18,18 @@ export class Pang extends Component {
         this.node.position = __pos;
     }
 
+    public Check_Collision = (__checkPosition: Vec3): boolean => {
+        let RoundSize = 130;
+        let Half_RoundSize = RoundSize * 0.5;
+        if(this.node.position.x - RoundSize < __checkPosition.x &&
+            this.node.position.x + RoundSize > __checkPosition.x &&
+            this.node.position.y - RoundSize < __checkPosition.y &&
+            this.node.position.y + RoundSize > __checkPosition.y
+            ) {//RoundSize로 계산하는건 해당 위치에 배치했을 때의 Pang 사이즈 감안해 [절반-절반] 감안
+            return true;
+        }
+        return false;
+    }
+
     // update(deltaTime: number) {}
 }
